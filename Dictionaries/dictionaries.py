44eb54f99575113950694406a11009e41c71dab2
Deyvit <deyvithernandez@gmail.com>
2018-06-16 17:33:51 -0500

    Section 9 Lecture 57 Before Challenge

--- Dictionaries/dictionaries.py
+++ Dictionaries/dictionaries.py
@@ -1,16 +1,16 @@
-fruit = {"orange": "a sweet, orange, citus fruit",
+fruit = {"orange": "a sweet, orange, citrus fruit",
          "apple": "good for making cider",
          "lemon": "a sour, yellow citrus fruit",
          "grape": "a small, sweet fruit growing in bunches",
          "lime": "a sour, green citrus fruit"}
 # "apple": "round and crunchy"}  # Last entry remains
 
-print(fruit)
+# print(fruit)
 # print(fruit["lemon"])
 
-fruit["pear"] = "an odd shaped apple"
+# fruit["pear"] = "an odd shaped apple"
 # print(fruit)
-fruit["lime"] = "great with tequila"
+# fruit["lime"] = "great with tequila"
 # print(fruit)
 # del fruit["lemon"]  # Only deletes the "lemon" entry
 # del fruit  # Deletes all the dictionary
@@ -60,7 +60,66 @@ fruit["lime"] = "great with tequila"
 #
 # for key in fruit:
 #     print(fruit[key])
-fruit_keys = fruit.keys()
-print(fruit_keys)
-fruit["tomato"] = "not nice with ice cream"
-print(fruit_keys)
+
+# fruit_keys = fruit.keys()
+# print(fruit_keys)
+# fruit["tomato"] = "not nice with ice cream"
+# print(fruit_keys)
+#
+# print(fruit)
+# print(fruit.items())
+# f_tuple = tuple(fruit.items())
+# print(f_tuple)
+#
+# for snack in f_tuple:
+#     item, description = snack
+#     print(item + " is " + description)
+#
+# print(dict(f_tuple))
+
+
+# JOIN EXAMPLES
+# myList = ["a", "b", "c", "d"]
+# letters = "sdafsafsdafdasfa"
+# # newString = ''
+# # for c in myList:
+# #     # newString += c + ", "  # very inneficient
+# #     newString = ", ".join(myList)
+#
+# newString = ", ".join(letters)
+#
+# print(newString)
+#
+# numbers = "123456789"
+# newString2 = " mississippi ".join(numbers)
+# print(newString2)
+
+locations = {0: "You are sitting in front of a computer learning Python",
+             1: "You are standing at the end of a road before a small brick building",
+             2: "You are at the top of a hill",
+             3: "You are inside a building, a well house for a small stream",
+             4: "You are in a valley beside a stream",
+             5: "You are in the forest"}
+
+exits = [{"Q": 0},
+         {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
+         {"N": 5, "Q": 0},
+         {"W": 1, "Q": 0},
+         {"N": 1, "W": 2, "Q": 0},
+         {"W": 2, "S": 1, "Q": 0}]
+
+loc = 1
+while True:
+    availableExits = ", ".join(exits[loc].keys())
+
+    print(locations[loc])
+
+    if loc == 0:
+        break
+
+    direction = input("Available exits are " + availableExits + ": ").upper()
+    print()
+    if direction in exits[loc]:
+        loc = exits[loc][direction]
+    else:
+        print("You cannot go in that direction")
