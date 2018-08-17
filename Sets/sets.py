33e0a1b736415b5826df0925d2a55cf0e9b185fd
DeyvitHernandez <deyvithernandez@gmail.com>
2018-08-17 05:15:29 -0500

    Lecture 61. Sets

---
+++ Sets/sets.py
@@ -0,0 +1,29 @@
+# farm_animals = {"sheep", "cow", "hen"}
+# print(farm_animals)
+#
+# for animal in farm_animals:
+#     print(animal)
+#
+# print("=" * 40)
+#
+# wild_animals = set(["lion", "tiger", "panther", "elephant", "hare"])
+# print(wild_animals)
+#
+# for animal in wild_animals:
+#     print(animal)
+#
+# farm_animals.add("horse")
+# wild_animals.add("horse")
+# print(farm_animals)
+# print(wild_animals)
+
+empty_set = set()
+empty_set2 = {}
+empty_set.add("a")
+# empty_set2.add("a")
+
+even = set(range(0, 40, 2))
+print(even)
+squares_tuple = (4, 6, 9, 16, 25)
+squares = set(squares_tuple)
+print(squares)
